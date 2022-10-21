9c98d9d2b8d27a6c73ea61fb79485dc15f317419
salsabilafnp <salsabilafebriantynp@gmail.com>
2022-10-21 14:58:18 +0700

    video 4 - laravel

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="css/style.css">
+@extends('layouts.main')
 
-  <title>Sabil's Blog | About</title>
-</head>
-<body>
-  <h1>Halaman About</h1>
-  <h3><?= $name; ?></h3>
-  <p><?= $email; ?></p>
-  <img src="img/<?= $image; ?>" alt="<?= $name; ?>" width="200">
-</body>
-</html>
+@section('container')
+    <h1>Halaman About</h1>
+
+    <h3>{{ $name }}</h3>
+    <p>{{ $email }}</p>
+    <img src="img/{{ $image }}" alt="{{ $name }}" width="200">
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,70 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "tittle" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "tittle" => "About",
         "name" => "Salsabila Febrianty",
         "email" => "203040104@mail.unpas.ac.id",
         "image" => "profil.jpg"
     ]);
 });
 
+
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "tittle" => "First Post",
+            "slug" => "first-post",
+            "author" => "Sabil",
+            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat quibusdam natus repudiandae voluptatem vero saepe perspiciatis amet porro velit, optio dolores excepturi sint nisi corporis recusandae, totam repellendus aut corrupti."
+        ],
+        [
+            "tittle" => "Second Post",
+            "slug" => "second-post",
+            "author" => "Ica",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et necessitatibus velit deleniti sunt in est explicabo magnam corrupti quo iusto laudantium, architecto nam aliquam omnis accusamus laboriosam numquam officia perferendis animi. Reiciendis error obcaecati tenetur officiis quas quidem, quos iusto, enim nobis est nostrum dolores quis vero harum cum dicta eum qui fugit sequi deleniti! Nulla molestiae et cupiditate in! Eius debitis provident beatae incidunt, perferendis, minima maxime earum blanditiis cupiditate quas aliquid error impedit! Maiores accusantium fugit obcaecati commodi, provident accusamus vitae blanditiis beatae modi deleniti iste. Vel officiis nisi distinctio, a reprehenderit dignissimos animi perspiciatis magni consequatur illo."
+        ]
+    ];
+
+    return view('posts', [
+        "tittle" => "Blog",
+        "posts" => $blog_posts
+    ]);
+});
+
+// page for single post
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "tittle" => "First Post",
+            "slug" => "first-post",
+            "author" => "Sabil",
+            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat quibusdam natus repudiandae voluptatem vero saepe perspiciatis amet porro velit, optio dolores excepturi sint nisi corporis recusandae, totam repellendus aut corrupti."
+        ],
+        [
+            "tittle" => "Second Post",
+            "slug" => "second-post",
+            "author" => "Ica",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et necessitatibus velit deleniti sunt in est explicabo magnam corrupti quo iusto laudantium, architecto nam aliquam omnis accusamus laboriosam numquam officia perferendis animi. Reiciendis error obcaecati tenetur officiis quas quidem, quos iusto, enim nobis est nostrum dolores quis vero harum cum dicta eum qui fugit sequi deleniti! Nulla molestiae et cupiditate in! Eius debitis provident beatae incidunt, perferendis, minima maxime earum blanditiis cupiditate quas aliquid error impedit! Maiores accusantium fugit obcaecati commodi, provident accusamus vitae blanditiis beatae modi deleniti iste. Vel officiis nisi distinctio, a reprehenderit dignissimos animi perspiciatis magni consequatur illo."
+        ]
+    ];
+
+    $new_post = [];
+    foreach($blog_posts as $post) {
+        if ($post['slug'] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "tittle" => "Single Post",
+        "post" => $new_post
+    ]);
 });
