d42f4672d219f22e27e9914b5c861d3f4497f3a7
salsabilafnp <salsabilafebriantynp@gmail.com>
2022-10-28 12:05:33 +0700

    video 5 - laravel

---
+++ app/Models/Post.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+
+class Post
+{
+    private static $blog_posts = [
+        [
+            "tittle" => "First Post",
+            "slug" => "first-post",
+            "author" => "Sabil",
+            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat quibusdam natus repudiandae voluptatem vero saepe perspiciatis amet porro velit, optio dolores excepturi sint nisi corporis recusandae, totam repellendus aut corrupti."
+        ],
+        [
+            "tittle" => "Second Post",
+            "slug" => "second-post",
+            "author" => "Ica",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et necessitatibus velit deleniti sunt in est explicabo magnam corrupti quo iusto laudantium, architecto nam aliquam omnis accusamus laboriosam numquam officia perferendis animi. Reiciendis error obcaecati tenetur officiis quas quidem, quos iusto, enim nobis est nostrum dolores quis vero harum cum dicta eum qui fugit sequi deleniti! Nulla molestiae et cupiditate in! Eius debitis provident beatae incidunt, perferendis, minima maxime earum blanditiis cupiditate quas aliquid error impedit! Maiores accusantium fugit obcaecati commodi, provident accusamus vitae blanditiis beatae modi deleniti iste. Vel officiis nisi distinctio, a reprehenderit dignissimos animi perspiciatis magni consequatur illo."
+        ]
+    ];
+
+    public static function all()
+    {
+        return collect(self::$blog_posts);
+    }
+
+    public static function find($slug)
+    {
+        $posts = static::all();
+        //$post = [];
+        //foreach ($posts as $p) {
+        //    if ($p['slug'] === $slug) {
+        //        $post = $p;
+        //    }
+        //}
+        return $posts->firstWhere('slug', $slug);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,56 +30,7 @@ Route::get('/about', function () {
     ]);
 });
 
-
-
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "tittle" => "First Post",
-            "slug" => "first-post",
-            "author" => "Sabil",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat quibusdam natus repudiandae voluptatem vero saepe perspiciatis amet porro velit, optio dolores excepturi sint nisi corporis recusandae, totam repellendus aut corrupti."
-        ],
-        [
-            "tittle" => "Second Post",
-            "slug" => "second-post",
-            "author" => "Ica",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et necessitatibus velit deleniti sunt in est explicabo magnam corrupti quo iusto laudantium, architecto nam aliquam omnis accusamus laboriosam numquam officia perferendis animi. Reiciendis error obcaecati tenetur officiis quas quidem, quos iusto, enim nobis est nostrum dolores quis vero harum cum dicta eum qui fugit sequi deleniti! Nulla molestiae et cupiditate in! Eius debitis provident beatae incidunt, perferendis, minima maxime earum blanditiis cupiditate quas aliquid error impedit! Maiores accusantium fugit obcaecati commodi, provident accusamus vitae blanditiis beatae modi deleniti iste. Vel officiis nisi distinctio, a reprehenderit dignissimos animi perspiciatis magni consequatur illo."
-        ]
-    ];
-
-    return view('posts', [
-        "tittle" => "Blog",
-        "posts" => $blog_posts
-    ]);
-});
+Route::get('/posts', [PostController::class, 'index']);
 
 // page for single post
-Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "tittle" => "First Post",
-            "slug" => "first-post",
-            "author" => "Sabil",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat quibusdam natus repudiandae voluptatem vero saepe perspiciatis amet porro velit, optio dolores excepturi sint nisi corporis recusandae, totam repellendus aut corrupti."
-        ],
-        [
-            "tittle" => "Second Post",
-            "slug" => "second-post",
-            "author" => "Ica",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et necessitatibus velit deleniti sunt in est explicabo magnam corrupti quo iusto laudantium, architecto nam aliquam omnis accusamus laboriosam numquam officia perferendis animi. Reiciendis error obcaecati tenetur officiis quas quidem, quos iusto, enim nobis est nostrum dolores quis vero harum cum dicta eum qui fugit sequi deleniti! Nulla molestiae et cupiditate in! Eius debitis provident beatae incidunt, perferendis, minima maxime earum blanditiis cupiditate quas aliquid error impedit! Maiores accusantium fugit obcaecati commodi, provident accusamus vitae blanditiis beatae modi deleniti iste. Vel officiis nisi distinctio, a reprehenderit dignissimos animi perspiciatis magni consequatur illo."
-        ]
-    ];
-
-    $new_post = [];
-    foreach($blog_posts as $post) {
-        if ($post['slug'] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "tittle" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
