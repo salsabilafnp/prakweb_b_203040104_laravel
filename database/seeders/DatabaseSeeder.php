dba94d811ece32891446c2ecead86502b54a8a74
salsabilafnp <salsabilafebriantynp@gmail.com>
2022-10-30 23:50:18 +0700

    video 9 - laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,64 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::create([
+            'name' => 'Chandra Arcychan Azfar',
+            'email' => 'chandraarcychan@gmail.com',
+            'password' => bcrypt('22mei2001')
+        ]);
+
+        User::create([
+            'name' => 'Salsabila Febrianty Nurul Putri',
+            'email' => 'salsabilafnp_@gmail.com',
+            'password' => bcrypt('27mei2001')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'category_id' => 1,
+            'user_id' => 1,
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, quo.',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores obcaecati laborum expedita exercitationem, rerum odio molestias consequuntur sit officiis quas!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam itaque, autem soluta molestiae eveniet ut blanditiis voluptatibus accusantium? Iste ut sapiente aliquid eveniet ex, quod amet aspernatur quo enim dolore.</p>'
+        ]);
+
+        Post::create([
+            'category_id' => 1,
+            'user_id' => 2,
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, quo.',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores obcaecati laborum expedita exercitationem, rerum odio molestias consequuntur sit officiis quas!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam itaque, autem soluta molestiae eveniet ut blanditiis voluptatibus accusantium? Iste ut sapiente aliquid eveniet ex, quod amet aspernatur quo enim dolore.</p>'
+        ]);
+
+        Post::create([
+            'category_id' => 2,
+            'user_id' => 1,
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, quo.',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores obcaecati laborum expedita exercitationem, rerum odio molestias consequuntur sit officiis quas!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam itaque, autem soluta molestiae eveniet ut blanditiis voluptatibus accusantium? Iste ut sapiente aliquid eveniet ex, quod amet aspernatur quo enim dolore.</p>'
+        ]);
+
+        Post::create([
+            'category_id' => 2,
+            'user_id' => 2,
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, quo.',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores obcaecati laborum expedita exercitationem, rerum odio molestias consequuntur sit officiis quas!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam itaque, autem soluta molestiae eveniet ut blanditiis voluptatibus accusantium? Iste ut sapiente aliquid eveniet ex, quod amet aspernatur quo enim dolore.</p>'
+        ]);
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,14 +2,17 @@
 
 @section('container')
     @foreach ($posts as $post)
-        <article class="mb-5">
-            <h2>
-                <a href="/posts/{{ $post->slug }}">
-                    {{ $post->title }}
-                </a>
+        <article class="mb-5 border-bottom pb-3">
+            <h2 class="text-decoration-none">
+                <a class="text-decoration-none" href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
             </h2>
+            <p>By.
+                <a class="text-decoration-none" href="#">{{ $post->user->name }}</a> <a class="text-decoration-none"
+                    href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+            </p>
             {{-- <h5>By: {{ $post['author'] }}</h5> --}}
             <p>{{ $post->excerpt }}</p>
+            <a class="text-decoration-none" href="/posts/{{ $post->slug }}">Read More...</a>
         </article>
     @endforeach
 @endsection
