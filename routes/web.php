5157d4d3b821a6249bda76b5693dac873cab99e0
salsabilafnp <salsabilafebriantynp@gmail.com>
2022-11-06 10:25:38 +0700

    laravel video 16 - login & middleware, formating tab

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
@@ -18,20 +19,20 @@ use App\Http\Controllers\RegisterController;
 */
 
 Route::get('/', function () {
-    return view('home', [
-        "tittle" => "Home",
-        "active" => 'home'
-    ]);
+  return view('home', [
+    "tittle" => "Home",
+    "active" => 'home'
+  ]);
 });
 
 Route::get('/about', function () {
-    return view('about', [
-        "tittle" => "About",
-        "active" => 'about',
-        "name" => "Salsabila Febrianty",
-        "email" => "203040104@mail.unpas.ac.id",
-        "image" => "profil.jpg"
-    ]);
+  return view('about', [
+    "tittle" => "About",
+    "active" => 'about',
+    "name" => "Salsabila Febrianty",
+    "email" => "203040104@mail.unpas.ac.id",
+    "image" => "profil.jpg"
+  ]);
 });
 
 Route::get('/posts', [PostController::class, 'index']);
@@ -41,16 +42,23 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 // categories
 Route::get('/categories', function () {
-    return view('categories', [
-        'tittle' => 'Post Categories',
-        "active" => 'categories',
-        'categories' => Category::all()
-    ]);
+  return view('categories', [
+    'tittle' => 'Post Categories',
+    "active" => 'categories',
+    'categories' => Category::all()
+  ]);
 });
 
 //login
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+
+//logout
+Route::post('/logout', [LoginController::class, 'logout']);
 
 //register
-Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
+
+//dashboard
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
